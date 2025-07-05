06837a095806f8d77bd38a2e796f0d41af41b0a4
Naga Venkata Venkata Manibabu <d.n.v.manibabu@gmail.com>
2025-07-05 13:32:54 +0530

    Add files via upload

---
+++ demo/src/main/java/controllayer/ProductController.java
@@ -0,0 +1,33 @@
+package controllayer;
+
+import Service.ProductService;
+import com.example.demo.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping("/api/products")
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        Product createdProduct = productService.createProduct(product);
+        return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return  productService.getAllProducts();
+
+    }
+}
